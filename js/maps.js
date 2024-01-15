5d251cc452e322594e6856cc328c2cba236c15aa
ChloeL-E <chloe_c123@hotmail.co.uk>
2024-01-15 20:22:37 +0000

    added markers to google map

--- js/maps.js
+++ js/maps.js
@@ -1,25 +1,39 @@
 
-      function initMap(){
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
-                center: {
-                    lat: 46.619261,
-                    lng: -33.134766
-                }
-            });
-            var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            var locations = [
-                {lat: 40.785091, lng: -73.968285},
-                {lat: 41.084045, lng: -73.874245},
-                {lat: 40.754932, lng: -73.984016}
-            ];
-            var markers = locations.map(function(location, i){
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
-                });
-            });
-            var markerClusterer = MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 40.78325271606445,
+            lng: -73.97003936767578
         }
-   
+    });
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874245 },
+        { lat: 40.754932, lng: -73.984016 }
+    ];
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    // Options to pass along to the marker clusterer
+    const clusterOptions = {
+        imagePath: "https:///Users/chloelivingstone-evans/Rosies-CV/images/images/m",
+        gridSize: 30,
+        zoomOnClick: false,
+        maxZoom: 10,
+    };
+
+    // Add a marker clusterer to manage the markers.
+    const markerClusterer = new MarkerClusterer(map, markers, clusterOptions);
+
+    // Change styles after cluster is created
+    const styles = markerClusterer.getStyles();
+    for (let i = 0; i < styles.length; i++) {
+        styles[i].textColor = "blue";
+        styles[i].textSize = 18;
+    }
+}
